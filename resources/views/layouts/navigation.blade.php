6be3d710441d1afd8a830eb439a4d705d532caf3
Jace <jt.scripter@gmail.com>
2024-05-10 18:22:30 +0200

    added envy logo to header

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,7 +5,7 @@
             <!-- Logo -->
             <div class="shrink-0">
                 <a href="{{ route('home') }}">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <x-application-logo class="block h-5 w-auto text-accent-600" />
                 </a>
             </div>
 
