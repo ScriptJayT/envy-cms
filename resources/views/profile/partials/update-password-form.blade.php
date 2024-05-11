0895a4519c3a0198fe0b6080bafd4ef4984e9918
Jace <jt.scripter@gmail.com>
2024-05-11 13:05:37 +0200

    ref reset pw form fields

--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -1,10 +1,9 @@
 <section>
     <header>
-        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+        <h2 class="text-lg font-medium">
             {{ __('Update Password') }}
         </h2>
-
-        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+        <p class="mt-1 text-sm text-theme-600">
             {{ __('Ensure your account is using a long, random password to stay secure.') }}
         </p>
     </header>
@@ -13,35 +12,23 @@
         @csrf
         @method('put')
 
-        <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
+        <x-form.text-field :_label="__('Current Password')" _action=toggle name=current_password type=password autocomplete="current-password">
             <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-        </div>
+        </x-form.text-field>
 
-        <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+        <x-form.text-field :_label="__('New Password')" _action=toggle name=password type=password autocomplete="new-password">
             <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
+        </x-form.text-field>
 
-        <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
+        <x-form.text-field :_label="__('Confirm Password')" _action=toggle name=password_confirmation type=password autocomplete="new-password">
             <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
+        </x-form.text-field>
 
         <div class="flex items-center gap-4">
             <x-primary-button>{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600 dark:text-gray-400"
-                >{{ __('Saved.') }}</p>
+            <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 2000)" class="text-sm">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
